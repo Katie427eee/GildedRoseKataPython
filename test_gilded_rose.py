4e009fe3e90830d75af9c688163430bba3915b08
miaoyue27 <shi.mia@northeastern.edu>
2025-02-05 22:29:18 -0800

    Lab4

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -7,9 +7,49 @@ from gilded_rose import Item, GildedRose
 class GildedRoseTest(unittest.TestCase):
     def test_foo(self):
         items = [Item("foo", 0, 0)]
+
+    # example of test that checks for logical errors
+    def test_sulfuras_should_not_decrease_quality(self):
+        items = [Item("Sulfuras", 5, 80)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEquals("fixme", items[0].name)
+
+        sulfuras_item = items[0]
+        self.assertEquals(80, sulfuras_item.quality)
+        self.assertEquals(4, sulfuras_item.sell_in)
+        self.assertEquals("Sulfuras", sulfuras_item.name)
+
+    # example of test that checks for syntax errors
+    def test_gilded_rose_list_all_items(self):
+        items = [Item("Sulfuras", 5, 80)]
+        gilded_rose = GildedRose(items)
+        all_items = gilded_rose.get_items()
+        self.assertEquals(["Sulfuras"], all_items)
+
+    def test_conjured_item_degrades_twice_as_fast(self):
+        items = [Item("Conjured Mana Cake", 3, 6)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(4, items[0].quality)
+
+    def test_aged_brie_quality_increase_after_sell_in(self):
+        items = [Item("Aged Brie", -1, 40)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(41, items[0].quality)
+
+    def test_sulfuras_quality_always_80(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 0, 50)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(80, items[0].quality)
+
+    def test_get_item_names_method_exists(self):
+        items = [Item("foo", 0, 0)]
+        gilded_rose = GildedRose(items)
+        names = gilded_rose.get_item_names()  # AttributeError raised here
+        self.assertEqual(["foo"], names)
 
 
 if __name__ == '__main__':
